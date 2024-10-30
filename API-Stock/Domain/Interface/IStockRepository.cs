66450b6333aead2dc4c05e219d630a042f7f90f4
LeonRonchi <leonbronchi@gmail.com>
2024-10-30 11:42:22 -0300

    feat: criou stock domain

---
+++ API-Stock/Domain/Interface/IStockRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Model;
+
+namespace Domain.Interface;
+
+public interface IStockRepository
+{
+    Task<Stock> SaveAsync(Stock stock);
+    Task<IEnumerable<Stock>> GetStockAsync();
+    Task<Stock?> GetStockByIdAsync(Guid id);
+}
---
+++ API-Stock/Domain/Model/Stock.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Exception;
+
+namespace Domain.Model;
+
+public class Stock
+{
+    public Guid? Id { get; set; }
+    public Guid IdProduct { get; set; }
+    public Guid IdSeller { get; set; }
+    public int Quantidade { get; set; }
+
+    public Stock (Guid? id, Guid idProduct, Guid idSeller, int quantidade)
+    {
+        ValidateDomain(quantidade);
+        Id = id;
+        IdProduct = idProduct;
+        IdSeller = idSeller;
+        Quantidade = quantidade;
+    }
+
+    private void ValidateDomain(int quantidade)
+    {
+        DomainExecptionValidation.When(quantidade < 0, "Quantidade menor que 0");
+    }
+}
